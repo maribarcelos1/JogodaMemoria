77819b8fe7ebc57edd72b70b26c4f8b83c6f0b1b
Mariana Barcelos <44508246+maribarcelos1@users.noreply.github.com>
2020-11-08 23:16:38 -0300

    Adicionando projeto.

---
+++ JogodaMemoria/JogoDaMemoria/JogoDaMemoria/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace JogoDaMemoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.lblMovimentos = new System.Windows.Forms.Label();
+            this.lblnome = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::JogoDaMemoria.Properties.Resources._001_computer;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 214);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "0";
+            this.pictureBox1.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::JogoDaMemoria.Properties.Resources._001_computer;
+            this.pictureBox2.Location = new System.Drawing.Point(230, 214);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "0";
+            this.pictureBox2.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::JogoDaMemoria.Properties.Resources._002_video_game;
+            this.pictureBox3.Location = new System.Drawing.Point(368, 214);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "1";
+            this.pictureBox3.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Image = global::JogoDaMemoria.Properties.Resources._002_video_game;
+            this.pictureBox4.Location = new System.Drawing.Point(506, 214);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "1";
+            this.pictureBox4.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox5.Image = global::JogoDaMemoria.Properties.Resources._003_palm_tree;
+            this.pictureBox5.Location = new System.Drawing.Point(644, 214);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "2";
+            this.pictureBox5.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox6.Image = global::JogoDaMemoria.Properties.Resources._003_palm_tree;
+            this.pictureBox6.Location = new System.Drawing.Point(782, 214);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "2";
+            this.pictureBox6.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox7.Image = global::JogoDaMemoria.Properties.Resources._004_dinosaur;
+            this.pictureBox7.Location = new System.Drawing.Point(92, 325);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "3";
+            this.pictureBox7.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox8.Image = global::JogoDaMemoria.Properties.Resources._004_dinosaur;
+            this.pictureBox8.Location = new System.Drawing.Point(230, 325);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "3";
+            this.pictureBox8.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox9.Image = global::JogoDaMemoria.Properties.Resources._005_cassette;
+            this.pictureBox9.Location = new System.Drawing.Point(368, 325);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "4";
+            this.pictureBox9.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox10.Image = global::JogoDaMemoria.Properties.Resources._005_cassette;
+            this.pictureBox10.Location = new System.Drawing.Point(506, 325);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Tag = "4";
+            this.pictureBox10.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox11.Image = global::JogoDaMemoria.Properties.Resources._006_game_controller;
+            this.pictureBox11.Location = new System.Drawing.Point(644, 325);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 10;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Tag = "5";
+            this.pictureBox11.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox12.Image = global::JogoDaMemoria.Properties.Resources._006_game_controller;
+            this.pictureBox12.Location = new System.Drawing.Point(782, 325);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(132, 105);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 11;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Tag = "5";
+            this.pictureBox12.Click += new System.EventHandler(this.ImagensClick_Click);
+            // 
+            // lblMovimentos
+            // 
+            this.lblMovimentos.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblMovimentos.AutoSize = true;
+            this.lblMovimentos.BackColor = System.Drawing.Color.Transparent;
+            this.lblMovimentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblMovimentos.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblMovimentos.Location = new System.Drawing.Point(86, 145);
+            this.lblMovimentos.Name = "lblMovimentos";
+            this.lblMovimentos.Size = new System.Drawing.Size(168, 31);
+            this.lblMovimentos.TabIndex = 12;
+            this.lblMovimentos.Text = "Movimentos:";
+            // 
+            // lblnome
+            // 
+            this.lblnome.AutoSize = true;
+            this.lblnome.BackColor = System.Drawing.Color.Transparent;
+            this.lblnome.Font = new System.Drawing.Font("Pristina", 50.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnome.ForeColor = System.Drawing.Color.Violet;
+            this.lblnome.Location = new System.Drawing.Point(268, 43);
+            this.lblnome.Name = "lblnome";
+            this.lblnome.Size = new System.Drawing.Size(427, 88);
+            this.lblnome.TabIndex = 13;
+            this.lblnome.Text = "Jogo da Memoria";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(984, 511);
+            this.Controls.Add(this.lblnome);
+            this.Controls.Add(this.lblMovimentos);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Memoria";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Label lblMovimentos;
+        private System.Windows.Forms.Label lblnome;
+    }
+}
+
---
+++ JogodaMemoria/JogoDaMemoria/JogoDaMemoria/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaMemoria
+{
+    public partial class Form1 : Form
+    {
+        int movimentos, cliques, cartasEncontradas, tagIndex;
+
+        Image[] img = new Image[6]; // vetor que vai guarda as imagens escolhidas em cada local
+
+        List<string> lista = new List<string>();
+
+        int[] tags = new int[2];
+
+        public Form1()
+        {
+            InitializeComponent();
+            inicio();
+        }
+
+        private void inicio()
+        {
+            foreach (PictureBox item in Controls.OfType<PictureBox>()) // cada item picturebox vai ser analisado por esse meio
+            {
+                tagIndex = int.Parse(String.Format("{0}", item.Tag)); // vai ler a imagem baseado no indice que ela se encontra
+                img[tagIndex] = item.Image; // vai receber a imagem que estava antes
+                item.Image = Properties.Resources._010_carnival_mask; // toda vez que iniciar o jogo as imagens ficaram padrao
+                item.Enabled = true;
+
+            }
+
+            Posicoes();
+        }
+
+        private void Posicoes()
+        {
+            foreach (PictureBox item in Controls.OfType<PictureBox>())
+            {
+                Random rdn = new Random();
+                int[] xP = { 92, 230, 368, 506, 644, 782 };
+                int[] yP = { 214, 325 };
+
+            Repete:
+                var X = xP[rdn.Next(0, xP.Length)];
+                var Y = yP[rdn.Next(0, yP.Length)];
+                
+                string verificacao = X.ToString() + Y.ToString();
+
+                if (lista.Contains(verificacao))// caso tiver volta
+                {
+
+                    goto Repete;
+
+                } else // caso nao tiver, adiciona
+
+                {
+                    item.Location = new Point(X, Y);
+                    lista.Add(verificacao);
+                }
+
+            }
+        }
+
+        private void ImagensClick_Click(object sender, EventArgs e)
+        {
+            bool parEncontrado = false;
+
+            PictureBox pic = (PictureBox)sender;
+            cliques++;
+
+            tagIndex = int.Parse(String.Format("{0}", pic.Tag));
+            pic.Image = img[tagIndex];
+            pic.Refresh();
+
+
+            if (cliques == 1) {
+                tags[0] = int.Parse(String.Format("{0}", pic.Tag));
+            }
+            else if (cliques == 2) {
+                movimentos++;
+                lblMovimentos.Text ="Movimentos: " + movimentos.ToString();
+                tags[1] = int.Parse(String.Format("{0}", pic.Tag));
+                parEncontrado = ChecagemPares();
+                desvira(parEncontrado);
+            }
+        }
+
+
+        private bool ChecagemPares()
+        {
+            cliques = 0;
+            if (tags[0] == tags[1]){
+
+                return true;
+
+
+            } else
+            {
+                return false;
+
+                
+            }
+            
+        }
+
+        private void desvira(bool check)
+        {
+
+            Thread.Sleep(500);
+            foreach (PictureBox item in Controls.OfType<PictureBox>())
+            {
+                if(int.Parse(String.Format("{0}", item.Tag)) == tags[0] ||
+                   int.Parse(String.Format("{0}", item.Tag)) == tags[1])
+                {
+                    if(check == true)
+                    {
+                        item.Enabled = false;
+                        cartasEncontradas++;
+
+                    }
+                    else
+                    {
+                        item.Image = Properties.Resources._010_carnival_mask;
+                        item.Refresh();
+                    }
+                }
+            }
+
+            finaljogo();
+        }
+
+        private void finaljogo()
+        {
+            if(cartasEncontradas == (img.Length * 2))
+            {
+                MessageBox.Show("Jogo terminado com " + movimentos.ToString() + " movimentos!", "Fim de Jogo");
+                DialogResult msg = 
+                MessageBox.Show("Deseja jogar novamente?", "Caixa de Pergunta", MessageBoxButtons.YesNo);
+
+                if(msg == DialogResult.Yes)
+                {
+
+                    cliques = 0; movimentos = 0; cartasEncontradas = 0;
+                    lista.Clear();
+                    inicio();
+
+                } else if (msg == DialogResult.No)
+
+                {
+                    MessageBox.Show("Obrigado por jogar!", "Agradecimento");
+                    Application.Exit();
+
+                }
+            }
+        }
+    }
+}
